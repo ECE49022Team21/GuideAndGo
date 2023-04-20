bc503d5a861f6535e1f3b87d15b76bd2823f2840
Chombler <64819823+Chombler@users.noreply.github.com>
2023-04-20 12:28:04 -0400

    Update state.c

--- stm32-deploy/Core/Src/state.c
+++ stm32-deploy/Core/Src/state.c
@@ -9,9 +9,9 @@ int get_state(){
     return curr_state - '0';
 }
 
-int set_state(int new_state, int curr_landmark, int next_landmark, int final_landmark){
+int set_state(int new_state, int curr_landmark, int next_landmark, int dest_landmark){
 	int state_length;
-    state_length = snprintf(state, STATE_SIZE, "%01d,%02d,%02d,%02d\n", new_state, curr_landmark, next_landmark, final_landmark);
+    state_length = snprintf(state, STATE_SIZE, "%01d,%02d,%02d,%02d\n", new_state, curr_landmark, next_landmark, dest_landmark);
     if(state_length != STATE_SIZE){
         return 0;
     }
@@ -27,5 +27,21 @@ int set_curr_landmark(int curr_landmark){
     int ones_place = curr_landmark % 10;
     state[2] = '0' + tens_place;
     state[3] = '0' + ones_place;
+    return 1;
+}
+
+int set_next_landmark(int next_landmark){
+    int tens_place = next_landmark / 10;
+    int ones_place = next_landmark % 10;
+    state[5] = '0' + tens_place;
+    state[6] = '0' + ones_place;
+    return 1;
+}
+
+int set_dest_landmark(int dest_landmark){
+    int tens_place = dest_landmark / 10;
+    int ones_place = dest_landmark % 10;
+    state[8] = '0' + tens_place;
+    state[9] = '0' + ones_place;
     return 1;
 }
